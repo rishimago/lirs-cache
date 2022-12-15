8789c7a6314fa2f1870becca4cc465675858559e
Aditya-Gollapudi <55288643+Aditya-Gollapudi@users.noreply.github.com>
2022-12-15 18:17:06 -0500

    FIFO

--- cache.py
+++ cache.py
@@ -14,6 +14,65 @@ class Disk:
       self.mem[key] = size
       return True, self.write_speed
 
+
+class FIFO:
+   def __init__(self,size,read_speed,write_speed,fallback,writethrough,add_reads_to_cache):
+        self.maxBytes = size
+        self.read_speed = read_speed
+        self.write_speed = write_speed
+        self.fallback = fallback
+        self.mem = {}
+        self.bytesStored = 0
+        self.recency_times = {}
+        self.writethrough = writethrough
+        self.num_accesses = 0
+        self.add_reads_to_cache = add_reads_to_cache
+
+    def read(self,key):
+        if(key in self.mem.keys()):
+            return True, True, self.read_speed
+        else:
+            succ, size, time = self.fallback.read(key)
+        return succ, False, (time + self.read_speed)
+
+
+    def write(self,key,size):
+      self.num_accesses += 1
+      self.last_access[key] = self.num_accesses
+      if(self.writethrough):
+        success, accrued_time = self.fallback.write(key, size)
+      else:
+        accrued_time = 0
+      
+      if(key in self.mem.keys()):
+          return True, True, self.write_speed + accrued_time
+      else:
+        while (self.bytesStored + size >= self.maxBytes):
+          if self.bytesStored == 0:
+            return False, False, self.write_speed + accrued_time
+
+          #find worst key
+          best_item = None
+          least_recent = infinity
+
+
+          for item, item_size in self.mem.items():
+            last = self.last_access[item]
+            if(last < least_recent):
+              least_recent = last
+              best_item = item 
+              best_size = item_size
+
+          self.mem.pop(best_item)
+          self.bytesStored -= best_size
+          if(not self.writethrough):
+            success,time = self.fallback.write(best_item, best_size)
+            accrued_time += time
+
+        self.mem[key] = size
+        self.bytesStored += size
+        return True, False, self.write_speed + accrued_time
+
 class LRU:
    def __init__(self,size,read_speed,write_speed,fallback,writethrough,add_reads_to_cache):
         self.maxBytes = size
