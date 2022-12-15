700c319a5b79748577ad58eb9df78454cba37607
Rishi Mago <9791341+rishimago@users.noreply.github.com>
2022-12-15 15:43:22 -0500

    Updated test trace script

--- test_trace.py
+++ test_trace.py
@@ -8,10 +8,13 @@ if __name__ == "__main__":
 
 	disk_read_speed = 10
 	disk_write_speed = 10
-	cache_size=5
+	cache_size = 5
 	cache_read_speed = 1
 	cache_write_speed = 1
-	cache_writethrough = True
+	cache_writethrough = False
+
+	percent_reads = 50
+	percent_writes = 100 - percent_reads
 
 	memory = Disk(disk_read_speed,disk_write_speed)
 	ram = LIRS(cache_size,cache_read_speed,cache_write_speed,memory,cache_writethrough)
@@ -23,17 +26,48 @@ if __name__ == "__main__":
 		read_times = []
 		write_times = []
 
+		hits = 0
+		misses = 0
+
+		lines = trace_file.readlines()
+
 		i = 0
-		for line in trace_file.readlines():
-			if i == 1000:
+		for line in lines:
+			if i == 50000:
 				break
+
 			timestamp, obj_id, obj_size = line.split(",")
 
-			is_read = random.choices([True, False], weights = [90, 10])[0]
+			is_read = random.choices([True, False], weights = [percent_reads, percent_writes])[0]
 
 			if is_read:
 				success, time = ram.read(obj_id)
 				read_times.append(time)
+
+				# if time == disk_read_speed + cache_read_speed:
+				# 	hits += 1
+			else:
+				success, time = ram.write(obj_id)
+				write_times.append(time)
+
+			successes.append(success)
+			i+=1
+
+		i = 0
+		for line in lines:
+			if i == 50000:
+				break
+
+			timestamp, obj_id, obj_size = line.split(",")
+
+			is_read = random.choices([True, False], weights = [percent_reads, percent_writes])[0]
+
+			if is_read:
+				success, time = ram.read(obj_id)
+				read_times.append(time)
+
+				# if time == disk_read_speed + cache_read_speed:
+				# 	hits += 1
 			else:
 				success, time = ram.write(obj_id)
 				write_times.append(time)
@@ -45,8 +79,8 @@ if __name__ == "__main__":
 
 	successes = np.array(successes)
 	print("Num Requests: ", count)
-	print("Num Reads: ", len(read_times))
-	print("Num Writes: ", len(write_times))
+	print("% Reads: ", len(read_times) / float(count))
+	print("% Writes: ", len(write_times) / float(count))
 	print("Success rate: ", len(successes[successes]) / float(count))
-	print("Average read time: ", np.average(read_times))
-	print("Average write time: ", np.average(write_times))
+	print("Read times – Mean: ", np.average(read_times), "Stdev: ", np.std(read_times))
+	print("Write times – Mean: ", np.average(write_times), "Stdev: ", np.std(write_times))
