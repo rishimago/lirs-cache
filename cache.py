2f51eb6957fb9a5d1b3bc77b8896873ff64a03d8
Aditya-Gollapudi <55288643+Aditya-Gollapudi@users.noreply.github.com>
2022-12-15 14:24:01 -0500

    cache?

--- cache.py
+++ cache.py
@@ -1,4 +1,4 @@
-from bisect import insort
+import sys import maxsize as infinity
 
 class Disk:
     def __init__ (self,read_speed,write_speed):
@@ -40,6 +40,8 @@ class LIRS:
         self.num_accesses += 1
         if(key in self.last_access.keys()):
           self.recency_times[key] = self.num_accesses - self.last_access[key]
+        else:
+          self.recency_times[key] = infinity
         self.last_access[key] = self.num_accesses
         if(self.writethrough):
           success, accrued_time = fallback.write(key)
@@ -48,3 +50,29 @@ class LIRS:
         
         if(key in self.mem):
             return True, self.write_speed + accrued_time
+        elif(not self.mem.full()):
+            self.mem.add(key)
+            return True, self.write_speed + accrued_time
+        else:
+          #find worst key
+          best_item = None
+          best_last = -1*infinity
+          best_recency = infinity
+          for item in self.mem:
+            last = self.last_access[item]
+            recency = self.recency_times[item]
+            if(recency < best_recency):
+              best_last = last 
+              best_recency = recency
+              best_item = item 
+            elif((recency == best_recency)and (last > best_last)):
+              best_last = last 
+              best_item = item 
+          self.mem.remove(best_item)
+          if(not writethrough):
+            success,accrued_time = fallback.write(best_item)
+          self.mem.add(key)
+          return True,  write_speed + accrued_time
+          
+
+          
